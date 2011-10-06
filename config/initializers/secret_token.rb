5559dfaf3cf35904567dc08ba5331ef0d6480570
Kaushalya Kularatnam <kaushalya.kularatnam@lbi.com>
2011-10-06 16:13:45 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaushismsApp::Application.config.secret_token = 'efaf700be72b76f4f4a9d93a64f450e968b3f83420ae297086d5ae58e1e9235f88c1678d0ef963b3ad06d5958de9ba9f936a8e819446e7a8307f24368d571094'
